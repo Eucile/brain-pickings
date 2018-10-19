eed9d3d0ec48196424535d3488aad1ba9b2f721f
Krystal Foster <kmichelefoster@gmail.com>
2018-10-19 11:28:18 -0700

    add and compose logo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { DonateComponent } from './donate/donate.component';
+import { LogoComponent } from './logo/logo.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DonateComponent,
+    LogoComponent,
+
   ],
   imports: [
     BrowserModule

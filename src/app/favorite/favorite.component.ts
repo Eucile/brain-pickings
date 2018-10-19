6b53e6089696c94237632fa28be51e69c5d5efb8
Krystal Foster <kmichelefoster@gmail.com>
2018-10-19 14:48:30 -0700

    add more sidebar features and start next component

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -8,8 +8,9 @@ import { Favorite } from '../models/favorite.model';
 })
 export class FavoriteComponent  {
   favorites: Favorite[] = [
-      new Favorite('<img src="https://i1.wp.com/www.brainpickings.org/wp-content/uploads/2017/10/bigwolflittlewolf00.jpg?zoom=2&fit=320%2C168&ssl=1">','The Best of Brain Pickings 2018'),
-      new Favorite('TITLE 2', '"QUOTE3"'),
-      new Favorite('TITLE 3', '"QUOTE3"')
+      new Favorite('https://i1.wp.com/www.brainpickings.org/wp-content/uploads/2017/10/bigwolflittlewolf00.jpg?zoom=2&fit=320%2C168&ssl=1','The Best of Brain Pickings 2018'),
+      new Favorite('https://i1.wp.com/www.brainpickings.org/wp-content/uploads/2018/05/mayaangelou_jannalevin1.jpg?zoom=2&fit=320%2C168&ssl=1', 'A Brave and Startling Truth: Astrophysicist Janna Levin Reads Maya Angelou’s Stunning Humanist Poem That Flew to Space, Inspired by Carl Sagan'),
+      new Favorite('https://i1.wp.com/www.brainpickings.org/wp-content/uploads/2016/06/thinslicesofanxiety0.jpg?zoom=2&fit=320%2C168&ssl=1', 'A Stoic’s Key to Peace of Mind: Seneca on the Antidote to Anxiety'),
+      new Favorite('https://i2.wp.com/www.brainpickings.org/wp-content/uploads/2014/01/mindset.jpg?zoom=2&fit=300%2C157&ssl=1', 'Fixed vs. Growth: The Two Basic Mindsets That Shape Our Lives')
     ];
 }

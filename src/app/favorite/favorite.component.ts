02515b51b957990cf28adef050e8b6870aec7eed
Krystal Foster <kmichelefoster@gmail.com>
2018-10-19 14:00:25 -0700

    experiment how to pass images and media via typescript

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -8,7 +8,7 @@ import { Favorite } from '../models/favorite.model';
 })
 export class FavoriteComponent  {
   favorites: Favorite[] = [
-      new Favorite('string','The Best of Brain Pickings 2018'),
+      new Favorite('<img src="https://i1.wp.com/www.brainpickings.org/wp-content/uploads/2017/10/bigwolflittlewolf00.jpg?zoom=2&fit=320%2C168&ssl=1">','The Best of Brain Pickings 2018'),
       new Favorite('TITLE 2', '"QUOTE3"'),
       new Favorite('TITLE 3', '"QUOTE3"')
     ];

d0f331990f9034f63a65fd6c26a4d56290f63643
Krystal Foster <kmichelefoster@gmail.com>
2018-10-26 10:23:08 -0700

    troubleshoot sending service to multiple components with differing models

--- src/app/article.service.ts
+++ src/app/article.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Article } from './models/article.model';
-import { ARTICLES } from './sample-articles';
+import { Favorite } from './models/favorite.model';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Injectable()
 export class ArticleService {
   articles: FirebaseListObservable<any[]>;
+  favorites: FirebaseListObservable<any[]>;
 
   constructor(private database: AngularFireDatabase) {
     this.articles = database.list('articles');
+    this.favorites = database.list('articles');
   }
   getArticles() {
     return this.articles;
+  }
+  getFavorites() {
+    return this.favorites;
   }
 }
--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Favorite } from '../models/favorite.model';
+import { ArticleService } from './article.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-favorite',
   templateUrl: './favorite.component.html',
-  styleUrls: ['./favorite.component.css']
+  styleUrls: ['./favorite.component.css'],
+  provider: [ArticleService]
 })
-export class FavoriteComponent  {
-  favorites: Favorite[] = [
-      new Favorite('https://i1.wp.com/www.brainpickings.org/wp-content/uploads/2017/10/bigwolflittlewolf00.jpg?zoom=2&fit=320%2C168&ssl=1','The Best of Brain Pickings 2018'),
-      new Favorite('https://i1.wp.com/www.brainpickings.org/wp-content/uploads/2018/05/mayaangelou_jannalevin1.jpg?zoom=2&fit=320%2C168&ssl=1', 'A Brave and Startling Truth: Astrophysicist Janna Levin Reads Maya Angelou’s Stunning Humanist Poem That Flew to Space, Inspired by Carl Sagan'),
-      new Favorite('https://i1.wp.com/www.brainpickings.org/wp-content/uploads/2016/06/thinslicesofanxiety0.jpg?zoom=2&fit=320%2C168&ssl=1', 'A Stoic’s Key to Peace of Mind: Seneca on the Antidote to Anxiety'),
-      new Favorite('https://i2.wp.com/www.brainpickings.org/wp-content/uploads/2014/01/mindset.jpg?zoom=2&fit=300%2C157&ssl=1', 'Fixed vs. Growth: The Two Basic Mindsets That Shape Our Lives')
-    ];
+export class FavoriteComponent implements OnInit  {
+  favorites: FirebaseListObservable<any[]>;
+  constructor(private database: AngularFireDatabase) {
+    ngOnInit(){
+      this.favorites = this.articleService.getFavorites();
+    }
+  }
+
+
+    //   new Favorite('https://i1.wp.com/www.brainpickings.org/wp-content/uploads/2017/10/bigwolflittlewolf00.jpg?zoom=2&fit=320%2C168&ssl=1','The Best of Brain Pickings 2018'),
+    //   new Favorite('https://i1.wp.com/www.brainpickings.org/wp-content/uploads/2018/05/mayaangelou_jannalevin1.jpg?zoom=2&fit=320%2C168&ssl=1', 'A Brave and Startling Truth: Astrophysicist Janna Levin Reads Maya Angelou’s Stunning Humanist Poem That Flew to Space, Inspired by Carl Sagan'),
+    //   new Favorite('https://i1.wp.com/www.brainpickings.org/wp-content/uploads/2016/06/thinslicesofanxiety0.jpg?zoom=2&fit=320%2C168&ssl=1', 'A Stoic’s Key to Peace of Mind: Seneca on the Antidote to Anxiety'),
+    //   new Favorite('https://i2.wp.com/www.brainpickings.org/wp-content/uploads/2014/01/mindset.jpg?zoom=2&fit=300%2C157&ssl=1', 'Fixed vs. Growth: The Two Basic Mindsets That Shape Our Lives')
+    // ];
 }

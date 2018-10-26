3d424d187fe260c567ea800b326ecc3b0f4ba3d4
Krystal Foster <kmichelefoster@gmail.com>
2018-10-26 10:32:23 -0700

    successfully link service and data to second component

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,22 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Favorite } from '../models/favorite.model';
-import { ArticleService } from './article.service';
+import { ArticleService } from '../article.service';
 import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-favorite',
   templateUrl: './favorite.component.html',
   styleUrls: ['./favorite.component.css'],
-  provider: [ArticleService]
+  providers: [ArticleService]
 })
 export class FavoriteComponent implements OnInit  {
   favorites: FirebaseListObservable<any[]>;
-  constructor(private database: AngularFireDatabase) {
-    ngOnInit(){
-      this.favorites = this.articleService.getFavorites();
-    }
-  }
+  constructor(private articleService: ArticleService) {}
 
+  ngOnInit(){
+    this.favorites = this.articleService.getFavorites();
+  }
 
     //   new Favorite('https://i1.wp.com/www.brainpickings.org/wp-content/uploads/2017/10/bigwolflittlewolf00.jpg?zoom=2&fit=320%2C168&ssl=1','The Best of Brain Pickings 2018'),
     //   new Favorite('https://i1.wp.com/www.brainpickings.org/wp-content/uploads/2018/05/mayaangelou_jannalevin1.jpg?zoom=2&fit=320%2C168&ssl=1', 'A Brave and Startling Truth: Astrophysicist Janna Levin Reads Maya Angelou’s Stunning Humanist Poem That Flew to Space, Inspired by Carl Sagan'),
